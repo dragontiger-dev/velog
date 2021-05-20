5065174419ca4793753b4d79e0be373557a52c44
dragontiger <dragontiger.dev@gmail.com>
2021-05-20 22:36:49 +0900

    Zip 압축풀기 테스트 코드 작성

--- src/test/java/io/velog/posting/zipInputStream/ZipInputStreamTest.java
+++ src/test/java/io/velog/posting/zipInputStream/ZipInputStreamTest.java
@@ -10,12 +10,16 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ZipInputStreamTest {
@@ -25,7 +29,7 @@ public class ZipInputStreamTest {
 
     @Test
     @DisplayName("zip 파일 다운로드")
-    public void getCorpCodeZipFile() throws InterruptedException, IOException {
+    public void downloadCorpCodeZipFile() throws InterruptedException, IOException {
 
         // Given
         RestTemplate restTemplate = new RestTemplate();
@@ -50,5 +54,37 @@ public class ZipInputStreamTest {
 
         // 테스트 후 삭제
         Files.delete(file);
+    }
+
+    @Test
+    @DisplayName("zip 파일 압축 파일 내 CORPCODE.xml 파일 추출")
+    public void getCorpCodeXmlFile() throws IOException {
+
+        // Given
+        Path zipFile = Opendart.downloadCorpCodeZip();
+        byte[] buf = Files.readAllBytes(zipFile);
+        ZipInputStream zipInputStream = new ZipInputStream(new ByteArrayInputStream(buf));
+        ZipEntry zipEntry = null;
+
+        String testDir = "C:\\Users\\dragontiger\\Desktop\\test\\";
+        String fileName = "CORPCODE.xml";
+
+        // 이미 존재하는 파일이 있으면 삭제
+        Path path = Path.of(testDir + fileName);
+        Files.deleteIfExists(path);
+
+        // When
+        while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+            long result = Files.copy(zipInputStream, Paths.get(testDir + zipEntry.getName()));
+        }
+        zipInputStream.closeEntry();
+        zipInputStream.close();
+
+        // Then
+        String[] fileNameArr = new File(testDir).list();
+        assertNotNull(fileNameArr);
+        assertEquals(fileName, fileNameArr[0]);
+
+        Files.delete(zipFile);
     }
 }
